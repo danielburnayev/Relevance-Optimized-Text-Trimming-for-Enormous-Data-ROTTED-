d120ee35df9414923298d25e120afe7e99e538fd
dburnaye <dburnaye@gmu.edu>
2026-02-14 04:09:56 -0500

    Simple formatting and included basic objects

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import TextFormElement from './TextFormElement';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    // outermost <> is a div with id root
+    <> 
+      <header className="flex items-center justify-between border-b-2 border-white min-w-[98vw] min-h-[10vh] pl-2.5 pr-2.5">
+        <h1>ROTTEN</h1>
+        <button id="settings-btn">Settings</button>
+      </header>
+
+      <form id="query-form" className="flex flex-col items-center min-w-full h-[90vh]">
+        <div className="flex items-center justify-center border-b-[0.75px] border-r-white w-[95%] h-[40%]">
+          <TextFormElement givenID="desired-outcome-text-field" labelText="Desired Outcome: " placeholderText="Wow"/>
+        </div>
+        <div className="flex flex-col items-center justify-center w-[95%] h-[60%]">
+          <h2>Accepts only one .zip file containing text files</h2>
+          <h3>Less than or equal to 20GB</h3>
+          <input type="file" id="file-upload" name="filename" accept=".zip" className="cursor-pointer border border-white"/>
+        </div>
+      </form>
+      
     </>
   )
 }
 
-export default App
+export default App;
---
+++ frontend/src/TextFormElement.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+interface GivenProps {
+    givenID: string;
+    labelText: string;
+    placeholderText: string;
+}
+
+function TextFormElement(props: GivenProps) {
+    const [backColorYes, setBackColorApperance] = useState(false);
+    
+    return (
+        <div className={`${(backColorYes) ? "bg-[#3f3f3f]" : ""} p-[5px]`}>
+            <label htmlFor={props.givenID}>{props.labelText}</label>
+            <input type="text" 
+                   id={props.givenID} placeholder={props.placeholderText} 
+                   onFocus={() => setBackColorApperance(true)}
+                   onBlur={() => setBackColorApperance(false)}/>
+        </div>
+    );
+}
+
+export default TextFormElement;
